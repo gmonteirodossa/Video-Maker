b81428ead888bd6c744b2b9fbe31b6b08167f578
Gabriel Monteiro dos Santos <gmonteirodossa@augusta.edu>
2019-05-05 23:49:00 -0400

    Added my project

---
+++ hello-world/index.js
@@ -0,0 +1,92 @@
+const fs = require('fs');
+
+const remote = require('electron').remote
+const main = remote.require('./main.js')
+const { ipcRenderer } = require('electron');
+
+var lyrics;
+function parseInputSub()
+{
+	var mysubtitles = document.getElementById('subtitles').value;
+	var index = 1;
+	var i = 0;
+	var start = 0;
+	var finish = 7;
+	var arrayofsub = mysubtitles.split("$");
+	for(var val of arrayofsub) {
+		val = index.toString() + "\r\n00:00:" + start.toString() + ",000 --> 00:00:" + finish.toString() + ",000\r\n" +  val.toString() + "\r\n";
+		arrayofsub[i] = val;
+		index += 1;
+		start += 8;
+		finish += 8;
+		i += 1;
+	}
+
+	lyrics = arrayofsub.join("");
+	console.log(lyrics);
+}
+
+ipcRenderer.on('videostatus', (event, message) => {
+	if(message == 'done')
+	{
+	button1.disabled = false;
+	button.disabled = false;
+	button2.disabled = false;
+	document.body.style.cursor='initial';
+	document.getElementById("message").innerHTML="Your video is ready.";
+	}
+	else if(message == 'preparing')
+	{
+	document.getElementById("message").innerHTML="Your video is being prepared...";
+	}
+})
+
+
+
+
+var button = document.getElementById('createvideo');
+button.addEventListener('click', () => {
+	parseInputSub();
+	fs.writeFile('blank.txt', lyrics, (err) => {  
+		// throws an error, you could also catch it here
+		if (err) throw err;
+	  
+		// success case, the file was saved
+		console.log('Subtitles saved!');
+	  });
+
+	
+	button1.disabled = true;
+	button.disabled = true;
+	button2.disabled = true;
+	document.body.style.cursor='wait';
+
+	if(main.createVideo() == null)
+	{
+	document.body.style.cursor='initial';
+	button1.disabled = false;
+	button.disabled = false;
+	button2.disabled = false;
+	}
+
+
+	
+}, false)
+
+
+var button1 = document.createElement('button')
+button1.classList.add("myButton");
+button1.textContent = 'select images'
+button1.addEventListener('click', () => {
+	main.openFile();
+}, false)
+document.body.appendChild(button1)
+
+var button2 = document.createElement('button')
+button2.classList.add("myButton");
+button2.textContent = 'select an audio'
+button2.addEventListener('click', () => {
+	main.openAudioFile();
+}, false)
+document.body.appendChild(button2)
+
---
+++ hello-world/main.js
@@ -0,0 +1,146 @@
+console.log('main process working');
+
+
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const url = require("url");
+const {ipcMain} = require('electron');
+let win;
+
+var selectedAudio;
+var selectedImages;
+const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
+const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+ffmpeg.setFfprobePath('ffprobe.exe');
+var ffprobe = require('ffprobe');
+module.exports = ffmpeg;
+var videoshow = require('videoshow');
+
+
+var options = {
+    fps: 25,
+    loop: 8,
+    transition: true,
+    transitionDuration: 2,
+    videoBitrate: 2048,
+    videoCodec: 'libx264',
+    size: '640x?',
+    audioBitrate: '128k',
+    audioChannels: 2,
+    format: 'mp4',
+    pixelFormat: 'yuv420p'
+}
+var createVideo = function createVideo() {
+
+if(!selectedAudio || !selectedImages)
+{
+  console.log("images or audio not selected");
+  return;
+}
+//else if (selectedImages.length > 8)
+videoshow(selectedImages, options)
+.audio(selectedAudio)
+.subtitles('blank.txt')
+.save('video.mp4')
+.on('start', function(command) {
+    console.log('iniciado processo ffmpeg:', command) 
+    win.webContents.send('videostatus', 'preparing');
+})
+.on('error', function(err, stdout, stderr){
+    console.error('Error:', err)
+    console.error('ffmpeg stderr:', stderr)
+
+})
+.on('end', function (output){
+    console.error('Video generated:', output);
+    win.webContents.send('videostatus', 'done');
+})
+}
+module.exports.createVideo = createVideo;
+var openFile = function openFile () {
+const { dialog } = require('electron');
+const fs = require('fs') ;
+  dialog.showOpenDialog({ filters: [
+
+    { name: 'images', extensions: ['jpg', 'png'] }
+ 
+   ], properties: ['multiSelections']} ,function (fileNames) {
+	if(fileNames === undefined) { 
+         console.log("No file selected"); 
+      
+      }
+      else 
+      {
+        selectedImages = fileNames;
+        console.log(selectedImages);
+      }
+  }); 
+
+}
+
+module.exports.openFile = openFile;
+
+var openAudioFile = function openAudioFile () {
+    const { dialog } = require('electron');
+    const fs = require('fs') ;
+      dialog.showOpenDialog({ filters: [
+    
+        { name: 'audio', extensions: ['mp3'] }
+     
+       ]} , function (fileName) {
+        if(fileName === undefined) { 
+
+             console.log("No file selected"); 
+          
+          }
+          else 
+          {
+            selectedAudio = fileName.toString();
+            console.log(selectedAudio);
+          }
+      }); 
+    
+    }
+    
+    module.exports.openAudioFile = openAudioFile;
+
+function createWindow() {
+	win = new BrowserWindow();
+	win.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file',
+	slashes: true}));
+	win.on('closed', () => {
+		win = null;
+	})
+}
+
+app.on('ready', createWindow);
+
+
+
+app.on('active', () => {
+	if (win === null){
+		createWindow()
+	}
+});
+
+exports.openWindow = () => {
+	let newwin = new BrowserWindow({width: 400, height: 200})
+	newwin.loadURL(url.format({
+		pathname: path.join(__dirname, 'example.html'),
+		protocol: 'file',
+	slashes: true}));
+	win.on('closed', () => {
+		win = null;
+	})
+}
+
+
+
+
+
+
